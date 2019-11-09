3c4a55eb4aa9647474bc4437eb6d80ee75f886cf
mayander30 <mayander30@gmail.com>
2019-11-09 11:34:51 -0300

    ajuste api

--- API/src/index.js
+++ API/src/index.js
@@ -1,218 +1,234 @@
-const { ApolloServer, gql, PubSub } = require('apollo-server')
-const Sequelize = require('./database')
-const User = require('./models/user')
-const Registered_Time = require('./models/registered_time')
+const { ApolloServer, gql, PubSub } = require('apollo-server');
+const Sequelize = require('./database');
+const User = require('./models/User')
+const Registered_Time = require('./models/Registered_Time')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const AuthDirective = require('./directives/auth')
 
-const pubSub = new PubSub()
+const pubSub = new PubSub();
 
+// The GraphQL schema
 const typeDefs = gql`
-    enum RoleEnum {
-        USER
-        WRITER
-    }
 
-    enum Roles {
-        ADMIN
-        FUNCIONARIO
-    }
+  enum Roles {
+    ADMIN
+    FUNCIONARIO
+  }
 
-    directive @auth(
-        role: RoleEnum
-    ) on OBJECT | FIELD_DEFINITION
+  directive @auth(
+    role: Roles
+  ) on OBJECT | FIELD_DEFINITION
 
-    type User {
-        id: ID!
-        name: String!
+
+  type Subscription {
+    onCreateUser: User
+  }
+
+	type User {
+    id: ID!
+		name: String!
+		email: String!
+		password: String
+		role: Roles
+    registered_times: [Registered_Time]
+	}
+
+  type Registered_Time {
+    id: ID!
+    time_registered: String!
+    user: User!    
+  }
+
+  type Query {
+    allRegistered_Times: [Registered_Time]
+    allUser: [User]
+    teste: String
+  }
+
+  type Mutation {
+    createRegistered_Time(data: CreateRegistered_TimeInput): Registered_Time
+    deleteRegistered_Time(id: ID!): Boolean @auth(role: ADMIN)
+
+    createUser(data: CreateUserInput): User @auth(role: ADMIN)
+    updateUser(id: ID!, data: CreateUserInput): User @auth(role: ADMIN)
+    deleteUser(id: ID!): Boolean @auth(role: ADMIN)
+
+    signin(
         email: String!
-        password: String
-        role: Roles
-        registered_times: [Registered_Time]
+        password: String!
+    ): PayloadAuth
+  }
+
+  type PayloadAuth {
+    token: String!
+    user: User!
+  }
+
+  input CreateRegistered_TimeInput {
+    time_registered: String!
+    user: CreateUserInput! 
+  }
+
+  input CreateUserInput {
+		name: String!
+		email: String!
+		password: String
+		role: Roles
+  }
+  
+  input UpdateUserInput {
+		name: String!
+		email: String!
+		password: String
+		role: Roles
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    async allRegistered_Times(parent, args, context) {
+
+      const user = await User.findByPk(context.userId);
+      
+      let where = {};
+
+      if (user.role !== 'ADMIN') {
+        where.userId = user.id;
+      }
+      
+      return Registered_Time.findAll({
+        include: [User],
+        where: where
+      });
+    },
+    allUser() {
+      return User.findAll({
+       include: [Registered_Time]
+      });
+    },
+    teste() {
+      return "oi";
+    }
+  },
+  Mutation: {
+    async createRegistered_Time(parent, body, context, info) {
+      console.log(body)
+      const [createUser, create] = await User.findOrCreate({
+        where: body.data.user
+      });
+
+      body.data.User = null;
+      const registered_time = await Registered_Time.create(body.data);
+      await registered_time.setUser(createUser.get('id'));
+      return registered_time.reload({ include: [User]});
+
+    },
+    async deleteRegistered_Time(parent, body, context, info) {
+      const registered_time = await Registered_Time.findOne({
+        id: body.id
+      });
+      await registered_time.destroy();
+      return true;
+    },
+
+    async createUser(parent, body, context, info) {
+      body.data.password = await bcrypt.hash(body.data.password, 10)
+      const w = await User.create(body.data);
+      const reloadW = w.reload({include: [Registered_Time]});
+      pubSub.publish('createdUser', { onCreateUser: reloadW });
+      return reloadW;
+    },
+    async updateUser(parent, body, context, info) {
+
+      if (body.data.password) {
+        body.data.password = await bcrypt.hash(body.data.password, 10)
+      }
+      const w = await User.findOne({
+        id: body.id
+      });
+      
+      if (!w) {
+        throw new Error('Usuario nao encontrado.');
+      }
+      const wAc = await w.update(body.data);
+
+      return wAc;
+    },
+    async deleteUser(parent, body, context, info) {
+      const w = await User.findOne({
+        id: body.id
+      });
+      await w.destroy();
+      return true;
+    },
+    async signin(parent, body, context, info) {
+      const user = await User.findOne({
+          where: { email: body.email }
+      })
+
+      if (user) {
+          const isCorrect = await bcrypt.compare(
+              body.password,
+              user.password
+          )
+          if (!isCorrect) {
+              throw new Error('Senha inválida')
+          }
+
+          const token = jwt.sign({ id: user.id }, 'secret')
+          
+          return {
+              token,
+              user
+          }
+      }
     }
 
-    type Registered_Time {
-        id: ID!
-        time_registered: String!
-        user: User!    
+  },
+  Subscription: {
+    onCreateUser: {
+      subscribe: () => pubSub.asyncIterator('createdUser')
     }
+  },
+  User: {
     
-
-    type Query {
-        allRegistered_Times: [Registered_Time]
-        allUser: [User]
-    }
-
-    type Mutation {
-        createRegistered_Time(data: CreateRegistered_TimeInput): Registered_Time @auth(role: WRITER)
-        deleteRegistered_Time(id: ID!): Boolean 
-
-        createUser(data: CreateUserInput): User
-        updateUser(id: ID! data: UpdateUserInput): User 
-        deleteUser(id: ID!): Boolean 
-
-        signin(
-            email: String!
-            password: String!
-        ): PayloadAuth
-    }
-
-    type PayloadAuth {
-        token: String!
-        user: User!
-    }
-
-    type Subscription {
-        onCreatedUser: User
-        onCreatedRegistered_Time: Registered_Time
-    }
-
-    input CreateRegistered_TimeInput {
-        time_registered: String!
-        user: CreateUserInput! 
-    }
-
-    input CreateUserInput {
-        name: String!
-        email: String!
-        password: String
-        role: Roles
-    }
-    
-    input UpdateUserInput {
-        name: String!
-        email: String!
-        password: String
-        role: Roles
-    }
-`
-
-const resolver = {
-    Query: {
-        allRegistered_Times() {
-            return Registered_Time.findAll({ include: [User] })
-        },
-        allUsers() {
-            return User.findAll({ include: [Registered_Time] })
-        }
-    },
-    Mutation: {
-        async createRegistered_Time(parent, body, context, info) {
-            if (body.data.user) {
-                const [createdUser, created] =
-                    await User.findOrCreate(
-                        { where: body.data.user }
-                    )
-                body.data.user = null
-                const registered_time = await Registered_Time.create(body.data)
-                await registered_time.setUser(createdUser.get('id'))
-                const newRegistered_Time = await registered_time.reload({ include: [User] })
-                pubSub.publish('createdRegistered_Time', {
-                    onCreatedRegistered_Time: newRegistered_Time
-                })
-                return newRegistered_Time
-
-            } else {
-                const registered_time = await Registered_Time.create(body.data, { include: [User] })
-                pubSub.publish('createdRegistered_Time', {
-                    onCreatedRegistered_Time: registered_time
-                })
-                return registered_time
-            }
-        },
-        async deleteRegistered_Time(parent, body, context, info) {
-            const registered_time = await Registered_Time.findOne({
-                where: { id: body.id }
-            })
-            await registered_time.destroy()
-            return true
-        },
-        async createUser(parent, body, context, info) {
-            body.data.password = await bcrypt.hash(body.data.password, 10)
-            const user = await User.create(body.data)
-            const reloadedUser = user.reload({ include: [Registered_Time] })
-            pubSub.publish('createdUser', {
-                onCreatedUser: reloadedUser
-            })
-            return reloadedUser
-        },
-        async updateUser(parent, body, context, info) {
-            if (body.data.password) {
-                body.data.password = await bcrypt.hash(body.data.password, 10)
-            }
-            const user = await User.findOne({
-                where: { id: body.id }
-            })
-            if (!user) {
-                throw new Error('Autor não encontrado')
-            }
-            const updateUser = await user.update(body.data)
-            return updateUser
-        },
-        async deleteUser(parent, body, context, info) {
-            const user = await User.findOne({
-                where: { id: body.id }
-            })
-            await user.destroy()
-            return true
-        },
-        async signin(parent, body, context, info) {
-            const user = await User.findOne({
-                where: { email: body.email }
-            })
-
-            if (user) {
-                const isCorrect = await bcrypt.compare(
-                    body.password,
-                    user.password
-                )
-                if (!isCorrect) {
-                    throw new Error('Senha inválida')
-                }
-
-                const token = jwt.sign({ id: user.id }, 'secret')
-
-                return {
-                    token,
-                    user
-                }
-            }
-        }
-    },
-    Subscription: {
-        onCreatedUser: {
-            subscribe: () => pubSub.asyncIterator('createdUser')
-        },
-        onCreatedRegistered_Time: {
-            subscribe: () => pubSub.asyncIterator('createdRegistered_Time')
-        }
-    },
-    User: {
-
-    }
-}
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolver,
-    schemaDirectives: {
-        auth: AuthDirective
-    },
-    context({ req, connection }) {
-        if (connection) {
-            return connection.context
-        }
+  typeDefs,
+  resolvers,
+  schemaDirectives: {
+    auth: AuthDirective
+  },
+  context({ req, connection }) {
+      if (connection) {
+          return connection.context
+      }
+      const token = req.headers.authorization
+      if (!token) {
+          throw new AuthenticationError('Você não está autorizado')
+      }
+      const jwtData = jwt.decode(token.replace('Bearer ', ''))
+      if(jwtData){
+        const { id } = jwtData
         return {
-            headers: req.headers
+            headers: req.headers,
+            userId: id
         }
-    }
+      }
+
+      return {
+        headers: req.headers
+      }
+  }
 });
 
-
 Sequelize.sync().then(() => {
-    server.listen()
-        .then(() => {
-            console.log('Servidor rodando')
-        })
-})
+
+  server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+});
